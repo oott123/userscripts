8fffd20d3eea2c8122eb1f46e328f142c2d6d69b
三三 <oott123@users.noreply.github.com>
2022-05-26 13:29:44 +0800

    Update mediawiki.language.fix.user.js

--- mediawiki.language.fix.user.js
+++ mediawiki.language.fix.user.js
@@ -2,8 +2,9 @@
 // @name        维基百科、萌娘百科链接语言修复 
 // @namespace   com.oott123.mediawiki.language.fix
 // @match       *://mzh.moegirl.org.cn/*
-// @match       *://zh.moegirl.org.cn/zh-*
-// @match       *://zh.wikipedia.org/zh-*
+// @match       *://zh.moegirl.org.cn/zh*
+// @match       *://zh.wikipedia.org/zh*
+// @match       *://zh.m.wikipedia.org/*
 // @grant       none
 // @version     1.0
 // @author      -
@@ -26,6 +27,11 @@ function fixLink() {
     newUrl.host = 'zh.moegirl.org.cn'
     changed = true
   }
+  if (newUrl.host === 'zh.m.wikipedia.org') {
+    newUrl.host = 'zh.wikipedia.org'
+    changed = true
+  }
+  console.log(newUrl)
 
   if (newUrl.pathname.startsWith('/zh') && !newUrl.pathname.startsWith('/zh-cn')) {
     newUrl.pathname = newUrl.pathname.replace(/^\/zh-?\w*/, '/zh-cn')
